841970243d98ccdde457517c5d38f589437e8b88
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-16 10:51:21 +0200

    Refactor main.py to improve code structure and add functionality for downloading and displaying NASA's Astronomy Picture of the Day.

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import streamlit as st
 from os import getenv
+import dotenv
 import requests
-from datetime import datetime, timedelta
-def main():
-    st.title("Nasa One day picture")
-    today = datetime.now()
-    yesterday = today - timedelta(days=1)
 
-    API_KEY = getenv('API_KEY')
-    URL = ""
+
+dotenv.load_dotenv()
+
+API_KEY: str = getenv("API_KEY")
+URL: str = f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}"
+FILE_PATH: str = "image.jpg"
+
+def load_image() -> None:
+    """
+    Downloads the image using the URL and saves it to a file.
+    """
+    content: dict[str, any] = requests.get(URL).json()
+    picture: str = content['hdurl']
+
+    with open(FILE_PATH, 'wb') as f:
+        f.write(requests.get(picture).content)
+
+def main() -> None:
+    """
+    Main function that displays the image and description on a web page.
+    """
+    st.title("Astronomy Picture of the Day")
+    content: dict[str, any] = requests.get(URL).json()
+    picture: str = content['hdurl']
+    description: str = content['explanation']
+
+    st.image(picture)
+    st.write("##")
+    st.write(description)
+    load_image()
+
 
 if __name__ == '__main__':
     main()
